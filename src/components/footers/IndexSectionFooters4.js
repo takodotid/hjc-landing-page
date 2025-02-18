c4e42d688d3f2acea671088cd0d75b4733f8b473
raflymln <19299378+raflymln@users.noreply.github.com>
2025-02-18 17:53:42 +0000

    Published from Shuffle Editor

---
+++ src/components/footers/IndexSectionFooters4.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default function IndexSectionFooters4() {
+    return (
+        <React.Fragment>
+            <>
+                <section className="relative bg-black overflow-hidden">
+  <div className="relative bg-black py-5">
+    <div className="container mx-auto px-4">
+      <div className="flex flex-wrap justify-between">
+        <p className="mb-8 md:mb-0 text-gray-500">© 2023-2025 PT Hobimu Jadi Cuan. All Rights Reserved.</p>
+        <div className="w-full md:w-auto">
+          <div className="flex flex-wrap">
+            <a className="mr-8" href="https://x.com/takodotid">
+              <img src="boldui-assets/brands/small-logos/twitter.svg" alt />
+            </a>
+            <a className="mr-8" href="https://www.instagram.com/takodotid">
+              <img src="boldui-assets/brands/small-logos/instagram.svg" alt />
+            </a>
+            <a className="mr-8" href="https://github.com/takodotid">
+              <img src="boldui-assets/brands/small-logos/github.svg" alt />
+            </a>
+            <a href="https://www.linkedin.com/company/takodotid">
+              <img src="boldui-assets/brands/small-logos/linkedin.svg" alt />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+
---
+++ src/components/logo-clouds/IndexSectionLogoClouds6.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function IndexSectionLogoClouds6() {
+    return (
+        <React.Fragment>
+            <>
+                <section id="products" className="pt-24 overflow-x-hidden relative">  <div className="container mx-auto px-4 mb-16">    <div className="max-w-xl mx-auto text-center">      <h2 className="mt-6 text-2xl text-white uppercase font-heading">WE ARE THE MASTERMIND BEHIND THIS GREAT PRODUCTS</h2>  </div></div>  <div className="flex flex-wrap text-neutral-800 justify-center border-y border-current">    <div className="w-1/2 md:w-1/3 lg:w-1/6 h-40 md:h-60">      <a className="flex h-full items-center justify-center border-x border-current" href="https://tako.id/">        <img className="h-7" src="https://static.shuffle.dev/uploads/files/89/895cf3ff10837dc8a6570639f0b5cefcca0ec364/Logotype-Blue.png" alt />    </a>  </div>    <div className="w-1/2 md:w-1/3 lg:w-1/6 h-40 md:h-60 border-current">      <a className="flex h-full items-center justify-center border-r border-current" href="#">        <img className="h-7" src="https://static.shuffle.dev/uploads/files/89/895cf3ff10837dc8a6570639f0b5cefcca0ec364/Group-1.png" alt />    </a>  </div></div></section>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+
