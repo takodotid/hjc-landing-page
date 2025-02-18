131058551b73fe60edbfc7c075f0bd87dfa5ec15
raflymln <19299378+raflymln@users.noreply.github.com>
2025-02-18 18:50:59 +0000

    Published from Shuffle Editor

---
+++ src/components/footers/IndexSectionFooters4.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function IndexSectionFooters4() {
+    return (
+        <React.Fragment>
+            <>
+                <section className="relative bg-black overflow-hidden">
+  <div className="relative bg-black py-5">
+    <div className="container mx-auto px-4">
+      <div className="flex flex-wrap justify-between">
+        <p className="mb-8 md:mb-0 text-gray-500">© 2023-2025 PT Hobimu Jadi Cuan. All Rights Reserved.</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+
