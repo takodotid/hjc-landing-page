2449bc062d2d21afb0a535743781a7416e3d13ab
raflymln <19299378+raflymln@users.noreply.github.com>
2025-02-18 17:47:35 +0000

    Published from Shuffle Editor

---
+++ src/components/newsletter/IndexSectionNewsletter5.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function IndexSectionNewsletter5() {
+    return (
+        <React.Fragment>
+            <>
+                <section id="contact" className="relative bg-black overflow-hidden border-b border-b-gray-800">  <div className="absolute top-0 right-0 w-2/3 md:w-1/2 lg:w-3/5 ml-auto h-64 md:h-full hidden md:block">    <img className="rounded-l-full w-full h-full object-cover" src="https://images.unsplash.com/photo-1635151947200-2368ad5e73d8?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMzIzMzB8MHwxfHNlYXJjaHwzMHx8dGVhbSUyMG5pZ2h0fGVufDB8fHx8MTczOTg5Njc0OHww&ixlib=rb-4.0.3&q=85&w=1920" alt /></div>  <div className="container mx-auto px-4">    <div className="w-full md:w-1/2 lg:w-1/3 py-32">      <h2 className="mb-8 text-2xl text-white uppercase font-heading">WE BELIEVE THAT COLLABORATION WILL CREATE MORE GREAT OPPORTUNITIES</h2>      <div>        <h3 className="text-lg text-blue-500">Address</h3>        <p className="text-lg text-white">Bellezza Office Tower Permata Hijau</p>        <p className="mb-6 text-lg text-white">South Jakarta 12210</p>        <h3 className="text-lg text-blue-500">Email</h3>        <a className="inline-block mb-8 text-lg text-white" href="mailto:company@hjc.co.id">company@hjc.co.id</a>    </div>  </div></div></section>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+
