af9768af778e9259c8888cb10ab8746a834cf12d
raflymln <19299378+raflymln@users.noreply.github.com>
2025-02-18 18:55:22 +0000

    Published from Shuffle Editor

---
+++ src/components/newsletter/IndexSectionNewsletter5.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function IndexSectionNewsletter5() {
+    return (
+        <React.Fragment>
+            <>
+                <section id="contact" className="relative bg-black overflow-hidden border-b border-b-gray-800 px-10">  <div className="absolute z-20 top-0 right-0 w-2/3 md:w-1/2 lg:w-3/5 ml-auto h-64 md:h-full hidden md:block">    <img className="w-full h-full object-cover object-left" src="https://static.shuffle.dev/uploads/files/89/895cf3ff10837dc8a6570639f0b5cefcca0ec364/horizon.svg" alt /></div>  <div className="container mx-auto px-4 z-20 relative">    <div className="w-full md:w-1/2 py-32">      <p className="text-lg font-semibold text-blue-400 pb-2 uppercase">Let's Collaborate</p>      <h2 className="mb-8 text-2xl text-white uppercase font-heading">WE BELIEVE THAT COLLABORATION WILL CREATE MORE GREAT OPPORTUNITIES</h2>      <div>        <h3 className="text-lg text-blue-500">Address</h3>        <p className="text-lg text-white">Bellezza Office Tower Permata Hijau</p>        <p className="mb-6 text-lg text-white">South Jakarta, Indonesia 12210</p>        <h3 className="text-lg text-blue-500">Email</h3>        <a className="inline-block mb-8 text-lg text-white" href="mailto:company@hjc.co.id">company@hjc.co.id</a>    </div>  </div></div>  <div className="absolute bottom-0 xl:-bottom-40 left-0 w-full z-10">    <img className="mx-auto h-full object-contain" src="boldui-assets/elements/shadows-light-blue.svg" alt /></div></section>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+
---
+++ src/pages/Index.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import IndexSectionHeaders1 from '../components/headers/IndexSectionHeaders1';
+import IndexSectionLogoClouds6 from '../components/logo-clouds/IndexSectionLogoClouds6';
+import IndexSectionNewsletter5 from '../components/newsletter/IndexSectionNewsletter5';
+import IndexSectionFooters4 from '../components/footers/IndexSectionFooters4';
+
+const meta = {
+  title: 'PT\u0020Hobimu\u0020Jadi\u0020Cuan',
+  meta: [],
+  link: [],
+  style: [],
+  script: [],
+};
+
+export default function Index() {
+  useEffect(() => {
+    // Custom CSS classes for elements from the index.html
+    const rootElement = document.getElementById('root');
+    classes = rootElement.classList;
+    rootElement.classList.remove(...classes);
+    rootElement.classList.add(...'bg-black'.split(' '));
+  });
+
+  return (
+    <React.Fragment>
+      <HelmetProvider>
+        <Helmet {...meta}></Helmet>
+      </HelmetProvider>
+      <IndexSectionHeaders1 />
+      <IndexSectionLogoClouds6 />
+      <IndexSectionNewsletter5 />
+      <IndexSectionFooters4 />
+    </React.Fragment>
+  );
+}
+
